1211e33c6a2398e3b107feae6aee7167b43e0676
revanthgss <g.revanth64@gmail.com>
2018-10-24 18:57:25 +0530

    Add map

--- alphacube/urls.py
+++ alphacube/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('ussd.urls')),
+    url(r'', include('evacroutes.urls')),
 ]
--- evacroutes/views.py
+++ evacroutes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from ussd.models import Victim
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    victims = Victim.objects.all()
+    presenttime = datetime.now()
+    return render(request, 'evacroutes/map.html', {'victims':list(victims),'present':presenttime})
