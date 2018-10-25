e7c8fff2e0d9f84626a146b5e20d284779545800
revanthgss <g.revanth64@gmail.com>
2018-10-25 18:30:42 +0530

    change static

--- alphacube/settings.py
+++ alphacube/settings.py
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-STATIC_ROOT = 'home/site/wwwroot/static'
+STATIC_ROOT = '/static'
