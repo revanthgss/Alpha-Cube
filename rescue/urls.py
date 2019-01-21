63d1f83c9a56ba258c069f5b3706f4ec8c97836a
M Pruthvi Taranath <39657647+pruthvi10t@users.noreply.github.com>
2019-01-21 15:15:49 +0530

    Update urls.py

--- rescue/urls.py
+++ rescue/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import include,url
+
+from django.contrib import admin
+from rescue.views import ProfileImageView
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^upload/$', views.upload, name='upload'),
-]
+    url(r'^profile_image_upload/', ProfileImageView.as_view(), name='profile_image_upload'),
+    ]
