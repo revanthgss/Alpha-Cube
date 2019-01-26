cf22dcc9b09ec424ced21bfd33f1b99ac8ce5d46
M Pruthvi Taranath <39657647+pruthvi10t@users.noreply.github.com>
2019-01-26 18:11:26 +0530

    Update urls.py

--- evacroutes/urls.py
+++ evacroutes/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^map/$', views.index, name='index'),
+    url(r'^victim/$', views.index, name='index'),
     url(r'^form/$', views.form, name='form'),
     url(r'^form/post/$', views.post, name='post'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^display/$', views.data, name='data')
+    url(r'^about/$', views.about, name='about')
 ]
